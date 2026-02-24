a684ffc3827120160995f95a7b1490d63d20e855
Davi Augusto <davi.miranda0919@gmail.com>
2026-02-24 15:32:23 -0300

    iniciado projeto com entity de usuário

---
+++ src/main/java/com/daviaugusto/projeto/login/entities/Usuario.java
@@ -0,0 +1,90 @@
+package com.daviaugusto.projeto.login.entities;
+
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private LocalDate dataNascimento;
+    @Column(unique = true)
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, LocalDate dataNascimento, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
---
+++ src/test/java/com/daviaugusto/projeto/login/ProjetoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daviaugusto.projeto.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
