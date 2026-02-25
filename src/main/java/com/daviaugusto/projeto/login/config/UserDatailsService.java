37550474b5980d11de58178963e4d1ac9a61a43c
Davi Augusto <davi.miranda0919@gmail.com>
2026-02-25 11:19:51 -0300

    adicionanando variaveis do banco e configuração do Spring Security

--- src/main/java/com/daviaugusto/projeto/login/config/UserDatailsService.java
+++ src/main/java/com/daviaugusto/projeto/login/config/UserDatailsService.java
@@ -1,4 +1,21 @@
 package com.daviaugusto.projeto.login.config;
 
-public class UserDatailsService {
+import com.daviaugusto.projeto.login.entities.Usuario;
+import com.daviaugusto.projeto.login.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDatailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
 }
