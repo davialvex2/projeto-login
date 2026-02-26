aa20659e4d44e2e6af8afc08c0b84291e311f8f6
Davi Augusto <davi.miranda0919@gmail.com>
2026-02-26 16:29:09 -0300

    Adicionando classe de configuração do Security

---
+++ src/main/java/com/daviaugusto/projeto/login/config/SecutiryConfig.java
@@ -0,0 +1,58 @@
+package com.daviaugusto.projeto.login.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecutiryConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDatailsService userDatailsService;
+
+    public SecutiryConfig(JwtUtil jwtUtil, UserDatailsService userDatailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDatailsService = userDatailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        JwtRequestFilter jwtRequestFilter = new JwtRequestFilter(jwtUtil, userDatailsService);
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/usuario").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuario/login").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session ->  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return new AuthenticationConfiguration().getAuthenticationManager();
+    }
+
+
+}
--- src/main/java/com/daviaugusto/projeto/login/service/UsuarioService.java
+++ src/main/java/com/daviaugusto/projeto/login/service/UsuarioService.java
@@ -5,12 +5,15 @@ import com.daviaugusto.projeto.login.dtos.request.UsuarioRequest;
 import com.daviaugusto.projeto.login.dtos.response.UsuarioResponse;
 import com.daviaugusto.projeto.login.entities.Usuario;
 import com.daviaugusto.projeto.login.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class UsuarioService {
 
     private UsuarioRepository usuarioRepository;
 
     private UsuarioConverter usuarioConverter;
+
+    private PasswordEncoder passwordEncoder;
 
 
     public UsuarioResponse salvarUsuario(UsuarioRequest usuarioRequest){
@@ -19,6 +22,7 @@ public class UsuarioService {
             throw new RuntimeException("Já existe um usuário com esse email");
         }
         else{
+            passwordEncoder.encode(usuario.getSenha());
             usuarioRepository.save(usuario);
             return usuarioConverter.paraUsuarioResponse(usuario);
         }
